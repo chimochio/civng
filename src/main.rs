d2dd48fcaa5709a95b82bf3b4d2072919cb2c49d
Virgil Dupras <hsoft@hardcoded.net>
2015-07-28 16:21:30 -0400

    Add GPLv3 license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/* Copyright 2015 Virgil Dupras
+ *
+ * This software is licensed under the "GPLv3" License as described in the "LICENSE" file,
+ * which should be included with this package. The terms are also available at
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 use std::path::Path;
 use rustty::{Event};
 use hexpos::{Pos, Direction};
